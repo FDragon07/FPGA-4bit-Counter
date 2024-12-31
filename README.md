3211a291e5b7540491dd46ae51be8be7a05221c3
FDragon07 <dylan@hagerfamily.com>
2024-12-30 18:02:15 -0800

    finished!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FPGA-4bit-Counter
+
+I am using a Cora Z7 07s version with Vivado as my IDE and VHDL as my programming language.
+
+![](IMG_6493.mov)
--- sources_1/new/counter.vhd
+++ sources_1/new/counter.vhd
@@ -21,47 +21,69 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity counter is
     Port ( 
         i_clk : in  STD_LOGIC;
-       -- i_reset : in  STD_LOGIC;
+        i_reset : in  STD_LOGIC;
         i_count : in STD_LOGIC;
-        o_LED_0 : out STD_LOGIC
-       -- o_LED_1 : out STD_LOGIC;
-       -- o_LED_2 : out STD_LOGIC;
-       -- o_LED_3 : out STD_LOGIC
+        o_LED_0 : out STD_LOGIC;
+        o_LED_1 : out STD_LOGIC;
+        o_LED_2 : out STD_LOGIC;
+        o_LED_3 : out STD_LOGIC
     );
 end counter;
 
 architecture Behavioral of counter is
+    -- Variables aka signals in VHDL
     signal r_count : STD_LOGIC := '0';
-    signal r_LED_0 : STD_LOGIC := '0';
-    --signal r_LED_1 : STD_LOGIC := '0';
-    --signal r_LED_2 : STD_LOGIC := '0';
-    --signal r_LED_3 : STD_LOGIC := '0';
+    signal r_reset : STD_LOGIC := '0';
+    signal r_Shift : STD_LOGIC_VECTOR(3 downto 0) := "0000";
+    signal r_debounce_counter : INTEGER range 0 to 10000000;
 
 begin
 
     process(i_clk)
     begin
         if rising_edge(i_clk) then
+            -- Debounce counter for both buttons because I am lazy and don't want to write two separate processes for each button
+            if r_debounce_counter > 0 then
+                r_debounce_counter <= r_debounce_counter - 1;
+            end if;
+
+            -- Count up button
             r_count <= i_count;
-            if r_count = '1' and i_count = '0' then
-                r_LED_0 <= not r_LED_0;
+            if r_count = '1' and i_count = '0' and r_debounce_counter = 0 then
+                r_debounce_counter <= 10000000;
+                if r_Shift(0) = '1' then
+                    if r_Shift(1) = '1' then
+                        if r_Shift(2) = '1' then
+                            if r_Shift(3) = '1' then
+                                r_Shift <= "0000";
+                            else
+                                r_Shift(3) <= not r_Shift(3);
+                            end if;
+                        end if;
+                        r_Shift(2) <= not r_Shift(2);
+                    end if;
+                    r_Shift(1) <= not r_Shift(1);
+                end if;
+                r_Shift(0) <= not r_Shift(0);
+            end if;
+            
+            -- Reset button
+            r_reset <= i_reset;
+            if r_reset = '1' and i_reset = '0' and r_debounce_counter = 0 then
+                r_debounce_counter <= 10000000;
+                r_Shift <= "0000";
             end if;
         end if;
     end process;
 
-    o_LED_0 <= r_LED_0;
+    o_LED_0 <= r_Shift(0);
+    o_LED_1 <= r_Shift(1);
+    o_LED_2 <= r_Shift(2);
+    o_LED_3 <= r_Shift(3);
 
 end Behavioral;
